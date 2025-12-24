ecd82ad8e23dd8df64c5ae258563a0c2beb2f823
Boishakhi G <bgmukta11@gmail.com>
2025-12-24 02:27:43 +0100

    initial structure added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ToastContainer} from 'react-toastify';
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='text-center text-red-400'>New</h1>
+      
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
